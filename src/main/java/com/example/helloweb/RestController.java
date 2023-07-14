cb04449efa7a8f80ad2ca066679816f2fea204ec
Arunlal MH (am2605) <am2605@att.com>
2023-07-14 14:32:35 +0530

    add new APIs

--- src/main/java/com/example/helloweb/RestController.java
+++ src/main/java/com/example/helloweb/RestController.java
@@ -2,12 +2,18 @@ package com.example.helloweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RestController {
 
-    @GetMapping("/sayhello")
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@RequestParam String name){
+        return "Hello " + name + "!";
     }
 }
